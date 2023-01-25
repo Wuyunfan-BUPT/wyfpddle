9b7b514ed6a54cdebc7a58c18a1ca07e3f830687
Wuyunfan-BUPT <1564396096@qq.com>
2023-01-25 13:56:31 +0800

    add unetplusplus

--- paddleseg/models/__init__.py
+++ paddleseg/models/__init__.py
@@ -66,3 +66,4 @@ from .lraspp import LRASPP
 from .mscale_ocrnet import MscaleOCRNet
 from .topformer import TopFormer
 from .rtformer import RTFormer
+from .swin_transformer_unet import SwinTransformerUnet
